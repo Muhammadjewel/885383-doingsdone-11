8b57001447bd1211b379e3cbd770e42f0b392734
muhammadjewel <msuratov@gmail.com>
2019-12-26 09:36:35 +0500

    WIP get tasks and projects from DB

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require_once('helpers.php');
 
+$currentUserId = 1;
+$getProjectsSql = 'SELECT * FROM projects WHERE projects.user_id = ?';
+$projects = dbFetchData($connection, $getProjectsSql, [$currentUserId]);
+$getTasksSql = 'SELECT * FROM tasks WHERE tasks.user_id = ?';
+$tasks = dbFetchData($connection, $getTasksSql, [$currentUserId]);
+var_dump($projects, $tasks);
+
 $pageContent = include_template('main.php', [
     'projects' => $projects,
     'tasks' => $tasks,
